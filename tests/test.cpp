ebce8cbe5d09fc48ff8a544df32daece9c040d7b
Petr Portnov <gh@progrm-jarvis.ru>
2020-12-26 23:53:19 +0300

    test: add a simple test for SimpleHttpCreate::from(..)

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,12 @@
 // Copyright 2020 Petr Portnov me@progrm-jarvis.ru
+
+#include <gtest/gtest.h>
+
+#include <http_client_lib/simple_http_client.hpp>
+
+namespace http_client_lib {
+
+    TEST(SimpleHttpClient, from__throws) {
+        EXPECT_THROW(SimpleHttpClient::from("some incorrect host", "8800"), SimpleHttpClient::exception_t);
+    }
+}
